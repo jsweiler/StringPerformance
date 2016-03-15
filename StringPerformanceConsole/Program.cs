3122be8e96e8ee7d0e9db1373e3c426f07c672c0
Jeff Weiler <jeffweiler@outlook.com>
2016-03-14 20:25:36 -0400

    added image

--- StringPerformanceConsole/Program.cs
+++ StringPerformanceConsole/Program.cs
@@ -12,37 +12,60 @@ namespace StringPerformanceConsole
         static void Main(string[] args)
         {
             //get things jitted properly
-            UseStringFormat(10000, "areallylongbiguglyextrasuperlongstring1", "areallylongbiguglyextrasuperlongstring2");
-            UseStringInterpolation(10000, "areallylongbiguglyextrasuperlongstring1", "areallylongbiguglyextrasuperlongstring2");
+            UseStringFormat(10000);
+            UseStringInterpolation(10000);
+            UseStringConcat(10000);
+            UseStringFormat(10000);
+            UseStringInterpolation(10000);
+            UseStringConcat(10000);
 
             var stopFormat = Stopwatch.StartNew();
-            UseStringFormat(5000000, "areallylongbiguglyextrasuperlongstring1", "areallylongbiguglyextrasuperlongstring2");
+            UseStringFormat(1000000);
             stopFormat.Stop();
             Console.WriteLine($"String format time: {stopFormat.Elapsed}");
 
             var stopInterpolation = Stopwatch.StartNew();
-            UseStringInterpolation(5000000, "areallylongbiguglyextrasuperlongstring1", "areallylongbiguglyextrasuperlongstring2");
+            UseStringInterpolation(1000000);
             stopInterpolation.Stop();
             Console.WriteLine($"String interpolation time: {stopInterpolation.Elapsed}");
+
+            var stopConcat = Stopwatch.StartNew();
+            UseStringConcat(1000000);
+            stopConcat.Stop();
+            Console.WriteLine($"String concatentation time: {stopConcat.Elapsed}");
 
             Console.ReadKey();
         }
 
-        public static void UseStringFormat(int number, string firstString, string secondString)
+                public static void UseStringFormat(int number)
         {
-            var array = new string[number + 1];
+            var random = new Random();
+            var text = "";
             for (var i = 0; i <= number; i++)
             {
-                array[i] = string.Format("First string {0} second string {1}", firstString, secondString);
+                text = string.Format("First string {0} second string {0}", random.Next(i, i + 1000), i);
             }
         }
 
-        public static void UseStringInterpolation(int number, string firstString, string secondString)
+        public static void UseStringInterpolation(int number)
         {
-            var array = new string[number + 1];
+            var random = new Random();
+            //var array = new string[number + 1];v
+            var text = "";
             for (var i = 0; i <= number; i++)
             {
-                array[i] = $"First string {firstString} second string {secondString}";
+                text = $"First string {random.Next(i, i + 1000)} second string {i}";
+            }
+        }
+
+        public static void UseStringConcat(int number)
+        {
+            var random = new Random();
+            //var array = new string[number + 1];
+            var text = "";
+            for (var i = 0; i <= number; i++)
+            {
+                text = "First string " + random.Next(i , i + 1000) + " second string " + i;
             }
         }
     }
